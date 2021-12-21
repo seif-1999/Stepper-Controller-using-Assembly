2d07c50e822613d907aa37c3f7981cd2c9d4dfc4
rowannaguib <95848695+rowannaguib@users.noreply.github.com>
2021-12-21 22:27:05 +0200

    Update FullStep.asm

--- FullStep.asm
+++ FullStep.asm
@@ -1,4 +1,5 @@
-  	 ; Step 1
+  	
+	; Step 1
 	 sbi PORTB, A1
 	 cbi PORTB, A2
 	 sbi PORTB, B1
@@ -19,8 +20,8 @@
 	 sbi PORTB, B2
 	 cbi PORTB, B1
    rcall delay
-
-
+	
+	
 	 ; Step 4
 	 sbi PORTB, A1
 	 cbi PORTB, A2
@@ -28,7 +29,7 @@
 	 cbi PORTB, B1
    rcall delay
 	 
-   
+   ;FUllStepReverse:
 	  ; Step 4
 	 sbi PORTB, A1
 	 cbi PORTB, A2
@@ -62,14 +63,31 @@
 	 cbi PORTB, B2
    rcall delay
    
-   delay:
-   MOV R1,#200D ; 200 x 50 ms = 10 sec
-   D1:
-   MOV TH1,#3CH
-   MOV TL1,#B0H
-   D2:
-   JNB TF1,D2 ; timer overflow in 50 ms
-   CLR TF1
-   DJNZ R1,D1 
-   ret
+ 	.equ l1		 = 20000  
+ delay:
+	  push r16
+	  push r17
+	  push r18
+	  push r19
+	  push r20
+	
+ loop2:
+    
+	  ldi r24, LOW(l1)
+	  ldi r25, HIGH(l1)
+	  
+loop1:
+	  sbiw r24, 1
+	  brne loop1
+
+	  dec delayMultiplier
+	  brne loop2
+
+	  pop r20
+	  pop r19
+	  pop r18
+	  pop r17
+	  pop r16
+
+	  ret
 	 
