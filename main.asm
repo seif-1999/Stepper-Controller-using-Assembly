111206401081552fece37208ca0dac3b093cdaff
Seifeldin5 <47761677+Seifeldin5@users.noreply.github.com>
2021-12-27 01:44:43 +0200

    may be last update

--- main.asm
+++ main.asm
@@ -1,11 +1,16 @@
-;
-; StepperController.asm
+;StepperController.asm
+; Created: 21/12/12 2:24:53 AM
+; Author : Seif
+
 ; Replace with your application code
 
 	.include "m32def.inc"		;load the ATmega32 directory which include rigsters' & bits' named and addresses
 
-
-	.def delayMultiplier = r19
+	
+	
+	.def ADCResL = r2
+	.def ADCResH = r3 ; r2, r3 must be saved
+	; define motor pins			
 	.equ A1				 = PB4
 	.equ A2				 = PB5
 	.equ B1				 = PB6
@@ -13,41 +18,93 @@
 
 
 
+
 	.cseg						;indicates that what follows is code
-	.org 0x00					;indicates orgin address in flash to start code segment
+	.org 0x00					;indicates orgin address in flash to start code 
+	rjmp main
+	.org 0x020
+	rjmp ADCDoneISR
+	.org 0x100
+
+	main:
+;*********** Stack intialization**********
+
+		ldi	r21,LOW(RAMEND)		; initialize 
+		out	SPL,r21				; stack pointer ;do not need to save r21
+		ldi	r21,HIGH(RAMEND)	; to RAMEND
+		out	SPH,r21				; "				;do not need to save r21
+;****************************
+		; set all portb as output
+		 ser r16
+		 out DDRB, r16 ;do not need to save r16
+		 out ddrd, r16
+;*************************
+
+		clr r16		; do not need to save r16
+		sts current, r16 ; intialize current position to 0
+						
+		ldi r16, 5		;do not need to save r16
+		sts delaySingleStepping, r16 ; intialize delay of single steps to 5
 
 
-	ldi	r21,LOW(RAMEND)		; initialize
-	out	SPL,r21				; stack pointer
-	ldi	r21,HIGH(RAMEND)	; to RAMEND
-	out	SPH,r21				; "
+	rcall 	ADCIntial
+
+
+
+
+
+	rcall ADCStartConv
 
 	
-
-	
-	 ser r16
-	 out DDRB, r16
-
-
 
 
 start:
-	 ldi r20, 100
-
-forward:
-	rcall  FullStep
-	dec r20
-	brne forward
-
-	ldi r20, 100
-reverse:
-	rcall FullStepReverse
-	dec r20
-	brne reverse
 
 
-	 
+	rjmp  posAdjst
+
  	rjmp start
+
+
+
+
+;*****************************
 
 	.include "delay.asm"
 	.include "FullStep.asm"
+	.include "HalfStep.asm"
+	.include "Division.asm"
+	.include "pot.asm"
+	.include "ADC.asm"
+	.include "Stack.asm"
+
+;*************ADC ISR******************
+	ADCDoneISR:
+
+	 push r16
+
+	
+
+			   ;read ADC here
+			  in ADCResL, ADCL
+			  in ADCResH, ADCH
+			  rcall ADCStartConv ; to start a new conversion as soon as the old one is done
+
+
+			 
+
+			  
+
+
+			  pop r16
+	reti
+
+;******************************
+
+
+	.dseg
+
+	
+	current: .byte 1 ; hold current position in steps
+	divResult: .byte 1 ; holds division result
+	delaySingleStepping: .byte 1
