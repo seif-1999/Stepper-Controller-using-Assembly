243e028a78354413d81c63910c1a11d8c7c6b84d
Seifeldin5 <47761677+Seifeldin5@users.noreply.github.com>
2022-01-01 04:36:26 +0200

    update

--- main.asm
+++ main.asm
@@ -3,7 +3,6 @@
 .include "m32def.inc"		;load the ATmega32 directory which include rigsters' & bits' named and addresses
 
 	
-	
 	.def ADCResL = r2
 	.def ADCResH = r3 ; r2, r3 must be saved
 	; define motor pins			
@@ -15,8 +14,8 @@
 
 
 
-	.cseg						;indicates that what follows is code
-	.org 0x00					;indicates orgin address in flash to start code 
+	.cseg					;directive defines the start of a Code segment
+	.org 0x00				;indicates orgin address in flash to start code 
 	rjmp main
 	.org 0x020
 	rjmp ADCDoneISR
@@ -26,21 +25,21 @@
 ;*********** Stack intialization**********
 
 		ldi	r21,LOW(RAMEND)		; initialize 
-		out	SPL,r21				; stack pointer ;do not need to save r21
+		out	SPL,r21			; stack pointer ;do not need to save r21
 		ldi	r21,HIGH(RAMEND)	; to RAMEND
-		out	SPH,r21				; "				;do not need to save r21
+		out	SPH,r21			; do not need to save r21
 ;****************************
 		; set all portb as output
 		 ser r16
-		 out DDRB, r16 ;do not need to save r16
+		 out DDRB, r16 			;do not need to save r16
 		 out ddrd, r16
 ;*************************
 
-		clr r16		; do not need to save r16
-		sts current, r16 ; intialize current position to 0
+		clr r16				; do not need to save r16
+		sts current, r16 		; intialize current position to 0
 						
-		ldi r16, 5		;do not need to save r16
-		sts delaySingleStepping, r16 ; intialize delay of single steps to 5
+		ldi r16, 5			;do not need to save r16
+		sts delaySingleStepping, r16    ; intialize delay of single steps to 5
 
 
 	rcall 	ADCIntial
@@ -62,8 +61,6 @@ start:
  	rjmp start
 
 
-
-
 ;*****************************
 
 	.include "delay.asm"
@@ -81,26 +78,22 @@ start:
 
 	
 
-			   ;read ADC here
+			  			;read ADC here
 			  in ADCResL, ADCL
 			  in ADCResH, ADCH
-			  rcall ADCStartConv ; to start a new conversion as soon as the old one is done
+			  rcall ADCStartConv 	; to start a new conversion as soon as the old one is done
 
 
 			 
-
-			  
-
-
+			 
 			  pop r16
 	reti
 
 ;******************************
 
 
-	.dseg
-
+	.dseg 					 ; directive defines the start of a Data segment
 	
-	current: .byte 1 ; hold current position in steps
-	divResult: .byte 1 ; holds division result
+	current: .byte 1			 ; hold current position in steps
+	divResult: .byte 1 			 ; holds division result
 	delaySingleStepping: .byte 1
