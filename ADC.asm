48260a4b98c8dfbe66f959c3a4c71b6530e54cf6
Seifeldin5 <47761677+Seifeldin5@users.noreply.github.com>
2022-01-07 19:19:01 +0200

    Add files via upload

--- ADC.asm
+++ ADC.asm
@@ -1,20 +1,12 @@
 
-
  ;****************************************ADC Intialization************************************
  ADCIntial:
 			push r16					; Save R16
-=======
-
-
- ADCIntial:
-			push r16
-
 			
 
 			; use r16 as a mask
 
-
-			ldi  r16,	 (1<<7)			; Enable ADC
+			ldi  r16,	 (1<<7)			; Enable ADC r16 = 0b 1000 0000 | 0b 0000 1000 = 1000 1000
 			ori  r16,	 (1<<3)			; ADC interrupt enable
 			ori  r16,	 (1<<2)			; ADC clock pre-scaler (/64)
 			ori  r16,	 (1<<1)			; ADC clock pre-scaler (/64)
@@ -26,7 +18,7 @@
 			
 		
 			in r16	,	SREG			; Enable
-			ori r16 ,   (1<<7)			; Global
+			ori r16 ,   (1<<7)			; Global (1<<7) = 0b10000000
 			out SREG,   r16				; Interrupts
 
 			pop r16						; Retrive R16
@@ -43,30 +35,3 @@
 			 ret
 
 ;**********************************************************************************************
-
-			ldi  r16, (1<<7) ; Enable ADC
-			ori   r16, (1<<3) ; ADC interrupt enable
-			ori   r16, (1<<2) ; ADC clock pre-scaler (/64)
-			ori   r16, (1<<1) ; ADC clock pre-scaler (/64)
-			out  ADCSRA, r16 ; Move this config to the ADC control register
-
-			
-			
-			sbi ADMUX, 5; left adjust the adc result to read high byte only for lower resolution
-			
-		
-			sbi SREG, 7
-
-			pop r16
-
- ret
-
-
- ADCStartConv:
- 
-			
-			 sbi ADCSRA, 6
-			 
- ret
-
-
